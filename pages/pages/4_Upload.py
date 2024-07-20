d44b92a829ea2d495f5ce3f1f1c574bb7b21ae14
SteeMod <stephenmodimakwane@gmail.com>
2024-07-20 05:31:12 +0000

    save

--- pages/pages/4_Upload.py
+++ pages/pages/4_Upload.py
@@ -35,7 +35,7 @@ if st.button('Submit'):
         st.write('File uploaded to Azure Blob Storage')
 
         # Now we send the file to Azure AI Document Intelligence
-        endpoint = "https://new1one.cognitiveservices.azure.com/"
+        endpoint = "https://new2two.cognitiveservices.azure.com/"
         apim_key = "1dc611154da945a39a3368b10fd088a7"
         model_id = "Luke1vs37"
         post_url = endpoint + "/formrecognizer/v3.1/custom/models/%s/analyze" % model_id
