9db4ab878cb95f0a23821a5ca992a1349a80b13c
SteeMod <stephenmodimakwane@gmail.com>
2024-07-20 19:02:56 +0000

    save

--- pages/pages/5_test.py
+++ pages/pages/5_test.py
@@ -12,6 +12,6 @@ def check_service_status(endpoint, key):
         return f"Service unavailable. Status code: {response.status_code}"
 
 # Replace 'your_endpoint' and 'your_key' with your actual values
-endpoint = "your_endpoint"
-key = "your_key"
+endpoint = "https://new1one.cognitiveservices.azure.com/"
+key = "1dc611154da945a39a3368b10fd088a7"
 print(check_service_status(endpoint, key))
