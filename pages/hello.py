4e8a77cb7b2294a607b121f983d0dce5d1f1515a
SteeMod <stephenmodimakwane@gmail.com>
2024-07-21 09:16:59 +0000

    save

--- pages/hello.py
+++ pages/hello.py
@@ -1,14 +1,31 @@
-
 import streamlit as st
-import base64
+from azure.storage.blob import BlobServiceClient
 
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
-    return href
+# Azure Storage Account details
+azure_storage_account_name = "devcareall"
+azure_storage_account_key = "GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA=="
+container_name = "data1"
+blob_name = "MOUD Tracker Form"  # name of the blob (file) in Azure Storage
 
-# Create a button to download the PDF file
-if st.button('Download PDF'):
-    st.markdown(get_binary_file_downloader_html('MOUD Tracker Form', 'PDF'), unsafe_allow_html=True)
+# Function to download file from Azure Storage
+def download_from_azure_storage():
+    blob_service_client = BlobServiceClient.from_connection_string(
+        f"DefaultEndpointsProtocol=https;AccountName={azure_storage_account_name};AccountKey={azure_storage_account_key}"
+    )
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    download_stream = blob_client.download_blob().readall()
+    return download_stream
+
+# Streamlit App
+st.title("Azure Storage Downloader")
+
+# Download the file from Azure Storage
+pdf_file = download_from_azure_storage()
+
+# Provide a download button in Streamlit
+st.download_button(
+    'Download PDF',
+    data=pdf_file,
+    file_name='downloaded_file.pdf',
+    mime='application/pdf'
+)
