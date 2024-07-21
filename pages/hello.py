e017250ca9c68f055cc00ffd9ee7ef53474e3bbb
SteeMod <stephenmodimakwane@gmail.com>
2024-07-21 08:46:12 +0000

    save

--- pages/hello.py
+++ pages/hello.py
@@ -3,26 +3,6 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-
-import streamlit as st
-import base64
-
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
-    return href
-
-# Create a button to download the PDF file
-if st.button('Download PDF'):
-    st.markdown(get_binary_file_downloader_html('MOUD Tracker Form', 'PDF'), unsafe_allow_html=True)
-
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
-
 st.write("# New Beginning")
 
 st.sidebar.success("Select a demo above.")
@@ -44,3 +24,16 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
+import streamlit as st
+import base64
+
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
+    return href
+
+# Create a button to download the PDF file
+if st.button('Download PDF'):
+    st.markdown(get_binary_file_downloader_html('MOUD Tracker Form', 'PDF'), unsafe_allow_html=True)
