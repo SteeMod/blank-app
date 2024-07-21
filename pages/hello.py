035eaf1a992ba815f3032da7caa5b6c7b7fcd5a4
SteeMod <stephenmodimakwane@gmail.com>
2024-07-21 08:22:14 +0000

    save

--- pages/hello.py
+++ pages/hello.py
@@ -1,11 +1,24 @@
 import streamlit as st
+import streamlit as st
+import base64
+
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{bin_file}">Download {file_label}</a>'
+    return href
+
+# Create a button to download the PDF file
+if st.button('Download PDF'):
+    st.markdown(get_binary_file_downloader_html('MOUD Tracker Form', 'PDF'), unsafe_allow_html=True)
 
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# New Beginning")
 
 st.sidebar.success("Select a demo above.")
 
