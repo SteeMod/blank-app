a430097afae771e627d90ede0b29bb2b4b7c4f8c
SteeMod <stephenmodimakwane@gmail.com>
2024-07-21 09:57:27 +0000

    save

--- pages/hello.py
+++ pages/hello.py
@@ -8,35 +8,6 @@ azure_storage_account_key = "GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqN
 container_name = "data1"
 blob_name = "MOUD Tracker Form.pdf"  # name of the blob (file) in Azure Storage
 
-# Function to download file from Azure Storage
-def download_from_azure_storage():
-    blob_service_client = BlobServiceClient.from_connection_string(
-        f"DefaultEndpointsProtocol=https;AccountName={azure_storage_account_name};AccountKey={azure_storage_account_key}"
-    )
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-    download_stream = blob_client.download_blob().readall()
-    return download_stream
-
-# Streamlit App
-st.title("Azure Storage Downloader")
-
-# Declare variable.
-if 'pdf_ref' not in st.session_state:
-    st.session_state.pdf_ref = None
-
-# Download the file from Azure Storage
-pdf_file = download_from_azure_storage()
-
-# Provide a download button in Streamlit
-if st.download_button(
-    'Download PDF',
-    data=pdf_file,
-    file_name='downloaded_file.pdf',
-    mime='application/pdf'
-):import streamlit as st
-from azure.storage.blob import BlobServiceClient
-from streamlit_pdf_viewer import pdf_viewer
-
 # Azure Storage Account details
 azure_storage_account_name = "your_storage_account_name"
 azure_storage_account_key = "your_storage_account_key"
@@ -69,7 +40,3 @@ st.download_button(
     file_name='downloaded_file.pdf',
     mime='application/pdf'
 )
-
-    # If the download button is clicked, display the PDF in a PDF viewer
-binary_data = pdf_file
-pdf_viewer(input=binary_data, width=700)
