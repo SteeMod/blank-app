534c1522d54d26b696a7d48fb6338b30ef8bf179
SteeMod <stephenmodimakwane@gmail.com>
2024-08-09 21:17:15 +0000

    .

--- pages/upload.test.py
+++ pages/upload.test.py
@@ -2,10 +2,10 @@ import streamlit as st
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 from azure.storage.blob import BlobServiceClient
+from csv import DictWriter
 from datetime import datetime
 import logging
 import os
-import json
 
 st.title("Upload Completed Form")
 
@@ -14,14 +14,14 @@ logging.basicConfig(level=logging.INFO)
 
 def main(uploaded_file):
     try:
-        endpoint = "https://new2two.cognitiveservices.azure.com/"
-        credential = AzureKeyCredential("027ad9245a594c5886cf5d90abecb9d1")
+        endpoint = os.getenv("AZURE_FORM_RECOGNIZER_ENDPOINT")
+        credential = AzureKeyCredential(os.getenv("AZURE_FORM_RECOGNIZER_KEY"))
         client = DocumentAnalysisClient(endpoint, credential)
 
         model_id = "Thessa5vs6"
 
         # Create BlobServiceClient
-        blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=devcareall;AccountKey=GEW0V0frElMx6YmZyObMDqJWDj3pG0FzJCTkCaknW/JMH9UqHqNzeFhF/WWCUKeIj3LNN5pb/hl9+AStHMGKFA==;EndpointSuffix=core.windows.net")
+        blob_service_client = BlobServiceClient.from_connection_string(os.getenv("AZURE_STORAGE_CONNECTION_STRING"))
         container_client = blob_service_client.get_container_client("data1")
 
         # Generate a timestamped filename
@@ -46,33 +46,36 @@ def main(uploaded_file):
             raise Exception("Expected at least one document in the result.")
         document = result.documents[0]
 
-        # Transform document.fields into a format suitable for JSON
-        record = {}
-        for key, field in document.fields.items():
-            if field.value_type == 'dictionary' and field.value and 'rows' in field.value:
-                # Check if the object is a table
-                if isinstance(field.value['rows'], list):
-                    # Handle table data
-                    for rowIndex, row in enumerate(field.value['rows']):
-                        if row and isinstance(row, list):
-                            for cellIndex, cell in enumerate(row):
-                                record[f"{key}_row{rowIndex}_cell{cellIndex}"] = cell.content
-            else:
-                # Handle regular fields
-                record[key] = field.content if field.content else field.value
+        # Create a CSV writer
+        csv_filename = f'result_{timestamp}.csv'
+        with open(csv_filename, mode='w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = [name for name in document.fields.keys()]
+            writer = DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
 
-        # Save the record as a JSON file
-        json_filename = f'result_{timestamp}.json'
-        with open(json_filename, 'w', encoding='utf-8') as jsonfile:
-            json.dump(record, jsonfile, ensure_ascii=False, indent=4)
+            # Transform document.fields into a format suitable for csv-writer
+            record = {}
+            for key, field in document.fields.items():
+                if field.value_type == 'dictionary' and field.value and 'rows' in field.value:
+                    # Check if the object is a table
+                    if isinstance(field.value['rows'], list):
+                        # Handle table data
+                        for rowIndex, row in enumerate(field.value['rows']):
+                            if row and isinstance(row, list):
+                                for cellIndex, cell in enumerate(row):
+                                    record[f"{key}_row{rowIndex}_cell{cellIndex}"] = cell.content
+                else:
+                    # Handle regular fields
+                    record[key] = field.content if field.content else field.value
+            writer.writerow(record)
 
-        # Create a new blob client for the JSON file
-        json_blob_name = f"CookedFiles/{json_filename}"
-        json_blob_client = container_client.get_blob_client(json_blob_name)
-        # Upload the JSON file to Azure Blob Storage
-        with open(json_filename, 'rb') as data:
-            json_blob_client.upload_blob(data, overwrite=True)
-        logging.info(f"JSON file '{json_blob_name}' uploaded successfully.")
+        # Create a new blob client for the CSV file
+        csv_blob_name = f"CookedFiles/{csv_filename}"
+        csv_blob_client = container_client.get_blob_client(csv_blob_name)
+        # Upload the CSV file to Azure Blob Storage
+        with open(csv_filename, 'rb') as data:
+            csv_blob_client.upload_blob(data, overwrite=True)
+        logging.info(f"CSV file '{csv_blob_name}' uploaded successfully.")
         
         # Display success message
         st.success("File uploaded successfully!")
