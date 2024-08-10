fa5db4e3c5990a7a9194d123d26804b1bd18518b
SteeMod <stephenmodimakwane@gmail.com>
2024-08-10 20:41:13 +0000

    .

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Medication Tracker Proof-of-Concept")
+st.write("Welcome and thank you for testing  this app! 😊")
+st.write("Kindly go to the Home Page, on mobile you should see a symbol like this <, tap on it")
