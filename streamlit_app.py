8c757891206f3bffb95f6cb7d4f8c6db756d22bc
SteeMod <stephenmodimakwane@gmail.com>
2024-08-10 20:43:18 +0000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title("Medication Tracker Proof-of-Concept")
 st.write("Welcome and thank you for testing  this app! 😊")
 st.write("Kindly go to the Home Page, on mobile you should see a symbol like this <, tap on it")
+ 
+ #Praise the Lord, my soul; all my inmost being, praise his holy name. Praise the Lord, my soul, and forget not all his benefits— Psalm 103:1-2
+ #The Lord is my shepherd, I lack nothing. He makes me lie down in green pastures, he leads me beside quiet waters, he refreshes my soul. He guides me along the right paths for his name’s sake. Even though I walk through the darkest valley, I will fear no evil, for you are with me; your rod and your staff, they comfort me. You prepare a table before me in the presence of my enemies. You anoint my head with oil; my cup overflows. Surely your goodness and love will follow me all the days of my life, and I will dwell in the house of the Lord forever. Psalm 23
+ #For I know the plans I have for you,” declares the Lord, “plans to prosper you and not to harm you, plans to give you hope and a future. Jeremiah 29:11
