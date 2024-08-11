16da6bb0dc48dca900966e5a2ecec8c61e89c67b
SteeMod <stephenmodimakwane@gmail.com>
2024-08-11 00:25:33 +0000

    🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦 ⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪ ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛ ⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪⚪ 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦

--- pages/test.py
+++ pages/test.py
@@ -3,6 +3,7 @@ from azure.storage.blob import BlobServiceClient
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 from csv import DictWriter
+from datetime import datetime
 import io
 import tempfile
 from streamlit_pdf_viewer import pdf_viewer
@@ -35,6 +36,12 @@ def display_pdf(pdf_data, selected_file):
         tmp_file.write(pdf_data.getbuffer())
         tmp_file_path = tmp_file.name
     pdf_viewer(tmp_file_path)
+    st.download_button(
+        label="Download PDF",
+        data=pdf_data,
+        file_name=selected_file,
+        mime="application/pdf"
+    )
 
 # Function to process uploaded file
 def process_uploaded_file(uploaded_file):
@@ -43,6 +50,17 @@ def process_uploaded_file(uploaded_file):
         credential = AzureKeyCredential(os.getenv("AZURE_FORM_RECOGNIZER_KEY"))
         client = DocumentAnalysisClient(endpoint, credential)
         model_id = "Thessa5vs6"
+
+        # Generate a timestamped filename
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        original_blob_name = uploaded_file.name
+        file_extension = os.path.splitext(original_blob_name)[1]
+        timestamped_blob_name = f"RawFiles/{timestamp}{file_extension}"
+
+        # Create a new blob client for the PDF file
+        pdf_blob_client = blob_service_client.get_blob_client(container_name, timestamped_blob_name)
+        pdf_blob_client.upload_blob(uploaded_file, overwrite=True)
+        logging.info(f"PDF file '{timestamped_blob_name}' uploaded successfully.")
 
         # Analyze the document from the uploaded file
         uploaded_file.seek(0)
@@ -53,7 +71,7 @@ def process_uploaded_file(uploaded_file):
         document = result.documents[0]
 
         # Create a CSV writer
-        csv_filename = 'out1.csv'
+        csv_filename = f'result_{timestamp}.csv'
         with open(csv_filename, mode='w', newline='', encoding='utf-8') as csvfile:
             fieldnames = [name for name in document.fields.keys()]
             writer = DictWriter(csvfile, fieldnames=fieldnames)
@@ -100,7 +118,7 @@ def main():
 
         # File uploader
         uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
-        submit_button = st.button('Upload')
+        submit_button = st.button('Submit')
         if uploaded_file is not None and submit_button:
             process_uploaded_file(uploaded_file)
 
