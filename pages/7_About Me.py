8ea937885b420ea502a5b2d74f5ed3d7ad37799e
SteeMod <stephenmodimakwane@gmail.com>
2024-08-20 20:10:34 +0000

    GOD is good

--- pages/7_About Me.py
+++ pages/7_About Me.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 from PIL import Image
 import io
 import os
@@ -8,8 +8,8 @@ def show_image():
     try:
         # Azure Blob Storage connection details from environment variables
         connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-        container_name = "data1"
-        blob_name = "photo.png"
+        container_name = "data1"  # Adjust container name
+        blob_name = "photo.png"   # Adjust blob name
 
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         blob_client = blob_service_client.get_blob_client(container_name, blob_name)
@@ -20,18 +20,19 @@ def show_image():
         return image_data
 
     except Exception as ex:
-        print('Exception:')
-        print(ex)
+        st.error(f"Error retrieving image: {ex}")
+        return None
 
 # Create two columns
 col1, col2 = st.columns(2)
 
 # Use the first column for text
-col1.title("About[Me]")
+col1.title("About Me")
 col1.write(
-    "Stephen Modimakwane is a Management Information Systems Specialist with over 18 years of experience in the field. His expertise include Data Analytics, Data Visualization, AI Infusion and Strategy Execution. He has a passion for creating technology solutions that help organizations and communities. Email: stephenmodimakwane@gmail.com"
+    "Stephen Modimakwane is a Management Information Systems Specialist with over 18 years of experience in the field. His expertise includes Data Analytics, Data Visualization, AI Infusion, and Strategy Execution. He has a passion for creating technology solutions that help organizations and communities. Email: stephenmodimakwane@gmail.com"
 )
 
 # Use the second column for the image
 image = show_image()
-col2.image(image, caption='Stephen Modimakwane', width=200)  # Adjust width as needed
+if image:
+    col2.image(image, caption='Stephen Modimakwane', width=200)
