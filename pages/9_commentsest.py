0181761d5b3404648950684bd4e415d56f0874ea
SteeMod <stephenmodimakwane@gmail.com>
2024-08-26 00:37:16 +0000

    Praise the LORD

--- pages/9_commentsest.py
+++ pages/9_commentsest.py
@@ -34,6 +34,6 @@ try:
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     comments = blob_client.download_blob().readall().decode('utf-8')
-    st.text_area(label='Comments', value=comments, height=200, max_chars=None, disabled=True)
+    st.markdown(f"### Comments\n{comments.replace('\n', '<br>')}", unsafe_allow_html=True)
 except Exception as e:
     st.error(f"An error occurred while fetching comments: {e}")
